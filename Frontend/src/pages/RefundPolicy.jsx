b49926e0c039e742ce1ad994d6000b93afbd7eb5
Anjali <ANJALIMISHRAPPS@GMAIL.COM>
2025-05-16 15:19:16 +0530

    p

--- Frontend/src/pages/RefundPolicy.jsx
+++ Frontend/src/pages/RefundPolicy.jsx
@@ -8,27 +8,44 @@ function RefundPolicy() {
 
       <h1 className="text-5xl font-black mb-12">Refund Policy</h1>
 
-      <div className="space-y-8">
-       <p>
-        This refund and cancellation policy outlines how you can cancel or seek a refund for a product / service that you have purchased through the Platform. Under this policy:
+     <div className="p-4 text-base leading-relaxed text-gray-700 space-y-4">
+  <p>
+    This refund and cancellation policy outlines how you can cancel or seek a refund for a product / service that you have purchased through the Platform. Under this policy:
+  </p>
 
-Cancellations will only be considered if the request is made 4 days after placing the order OR if the service you paid for is not availed. However, cancellation requests for products may not be entertained if the orders have been communicated to such sellers / merchant(s) listed on the Platform and they have initiated the process of shipping them, or the product is out for delivery. In such an event, you may choose to reject the product at the doorstep.
+  <p>
+    Cancellations will only be considered if the request is made 4 days after placing the order OR if the service you paid for is not availed. However, cancellation requests for products may not be entertained if the orders have been communicated to such sellers / merchant(s) listed on the Platform and they have initiated the process of shipping them, or the product is out for delivery. In such an event, you may choose to reject the product at the doorstep.
+  </p>
 
-DOGGOS HEAVEN PRIVATE LIMITED does not accept cancellation requests for perishable items like flowers, eatables, etc. However, the refund / replacement can be made if the user establishes that the quality of the product delivered is not good or if the bought service is not availed.
+  <p>
+    DOGGOS HEAVEN PRIVATE LIMITED does not accept cancellation requests for perishable items like flowers, eatables, etc. However, the refund / replacement can be made if the user establishes that the quality of the product delivered is not good or if the bought service is not availed.
+  </p>
 
- DOGGOS HEAVEN PRIVATE LIMITED does not accept cancellation requests for perishable items like toys, unsealed shampoos, etc.
+  <p>
+    DOGGOS HEAVEN PRIVATE LIMITED does not accept cancellation requests for perishable items like toys, unsealed shampoos, etc.
+  </p>
 
-In case of products , on receiving of damaged or defective items, please report to our customer service team. The request would be entertained once the seller/ merchant listed on the Platform, has checked and determined the same at its own end. This should be reported within 4 days of receipt of products. In case you feel that the product received is not as shown on the site or as per your expectations, you must bring it to the notice of our customer service within 4 days of receiving the product. The customer service team after looking into your complaint will take an appropriate decision. The product must be returned in good condition and with all original documentation.
+  <p>
+    In case of products , on receiving of damaged or defective items, please report to our customer service team. The request would be entertained once the seller/ merchant listed on the Platform, has checked and determined the same at its own end. This should be reported within 4 days of receipt of products. In case you feel that the product received is not as shown on the site or as per your expectations, you must bring it to the notice of our customer service within 4 days of receiving the product. The customer service team after looking into your complaint will take an appropriate decision. The product must be returned in good condition and with all original documentation.
+  </p>
 
-To cover the costs associated with any product/ service the following refund policy applies:
-a) Refund below ₹20,000: If a product or untilised service below ₹20,000 is returned within 4 days of purchase, an amount of ₹5,000 will be deducted from the refund.
-b) Refunds at ₹20,000 and Above: If a product or untilised service at ₹20,000 or above is returned within 4 days of purchase, an amount of ₹7,500 will be deducted from the refund.
+  <p>
+    To cover the costs associated with any product/ service the following refund policy applies:
+    <br />
+    a) Refund below ₹20,000: If a product or untilised service below ₹20,000 is returned within 4 days of purchase, an amount of ₹5,000 will be deducted from the refund.
+    <br />
+    b) Refunds at ₹20,000 and Above: If a product or untilised service at ₹20,000 or above is returned within 4 days of purchase, an amount of ₹7,500 will be deducted from the refund.
+  </p>
 
-In case of complaints regarding the products that come with a warranty from the manufacturers, please refer the issue to them.
+  <p>
+    In case of complaints regarding the products that come with a warranty from the manufacturers, please refer the issue to them.
+  </p>
 
-In case of any refunds approved by DOGGOS HEAVEN PRIVATE LIMITED, it will take 15 days for the refund to be processed to you.
-       </p>
-      </div>
+  <p>
+    In case of any refunds approved by DOGGOS HEAVEN PRIVATE LIMITED, it will take 15 days for the refund to be processed to you.
+  </p>
+</div>
+
     </div>
   )
 }
