ebf5ca3b7fd4b9641da654f9aa099d5a8acf6f2e
Anjali <ANJALIMISHRAPPS@GMAIL.COM>
2025-06-13 20:24:23 +0530

    pet phone. no.

--- Frontend/src/component/PetOwnerMaster/PetForm.jsx
+++ Frontend/src/component/PetOwnerMaster/PetForm.jsx
@@ -5,7 +5,8 @@ import { useDispatch, useSelector } from "react-redux";
 
 const PetForm = () => {
   const { addPetLoading } = useSelector((state) => state.pets);
-  const { register, handleSubmit, reset } = useForm();
+  const { register, handleSubmit, reset, formState: { errors },
+} = useForm();
   const [pets, setPets] = useState([{ vaccinations: [] }]);
   const dispatch = useDispatch();
 
@@ -101,322 +102,7 @@ const PetForm = () => {
     setPets(newPets);
   };
 
-  // if (addPetLoading) {
-  //   return (
-  //     <div className="flex justify-center items-center h-screen">
-  //       <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-blue-600"></div>
-  //     </div>
-  //   );
-  // } else
-  //   return (
-  //     <div className="max-w-2xl mx-auto p-6 bg-white rounded-lg shadow-md">
-  //       <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
-  //         <h3 className="text-xl font-semibold text-gray-800 mt-8 mb-4">
-  //           Owner Information
-  //         </h3>
 
-  //         <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-  //           <div>
-  //             <label
-  //               htmlFor="ownerName"
-  //               className="block text-sm font-medium text-gray-700"
-  //             >
-  //               Owner Name
-  //             </label>
-  //             <input
-  //               type="text"
-  //               id="ownerName"
-  //               {...register("ownerName", { required: true })}
-  //               className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-  //             />
-  //           </div>
-
-  //           <div>
-  //             <label
-  //               htmlFor="phone"
-  //               className="block text-sm font-medium text-gray-700"
-  //             >
-  //               Phone
-  //             </label>
-  //             <input
-  //               type="tel"
-  //               id="phone"
-  //               {...register("phone", {
-  //                 required: true,
-  //                 pattern: /^[0-9]{10}$/,
-  //               })}
-  //               className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-  //             />
-  //           </div>
-
-  //           <div>
-  //             <label
-  //               htmlFor="email"
-  //               className="block text-sm font-medium text-gray-700"
-  //             >
-  //               Email
-  //             </label>
-  //             <input
-  //               type="email"
-  //               id="email"
-  //               {...register("email")}
-  //               className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-  //             />
-  //           </div>
-
-  //           <div>
-  //             <label
-  //               htmlFor="segment"
-  //               className="block text-sm font-medium text-gray-700"
-  //             >
-  //               Segment
-  //             </label>
-  //             <select
-  //               id="segment"
-  //               {...register("segment", { required: true })}
-  //               className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-  //             >
-  //               <option value="Day Care">Day Care</option>
-  //               <option value="Veterinary">Veterinary</option>
-  //             </select>
-  //           </div>
-  //         </div>
-
-  //         <div>
-  //           <label
-  //             htmlFor="address"
-  //             className="block text-sm font-medium text-gray-700"
-  //           >
-  //             Address
-  //           </label>
-  //           <textarea
-  //             id="address"
-  //             {...register("address", { required: true })}
-  //             rows={3}
-  //             className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-  //           />
-  //         </div>
-  //         <h2 className="text-2xl font-bold text-gray-800 mb-6">
-  //           Pet Information
-  //         </h2>
-
-  //         {pets?.map((pet, petIndex) => (
-  //           <div key={petIndex}>
-  //             <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-  //               <div>
-  //                 <label
-  //                   htmlFor={`name_${petIndex}`}
-  //                   className="block text-sm font-medium text-gray-700"
-  //                 >
-  //                   Name
-  //                 </label>
-  //                 <input
-  //                   type="text"
-  //                   id={`name_${petIndex}`}
-  //                   {...register(`pets[${petIndex}].name`, { required: true })}
-  //                   className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-  //                 />
-  //               </div>
-
-  //               <div>
-  //                 <label
-  //                   htmlFor={`species_${petIndex}`}
-  //                   className="block text-sm font-medium text-gray-700"
-  //                 >
-  //                   Species
-  //                 </label>
-  //                 <select
-  //                   id={`species_${petIndex}`}
-  //                   {...register(`pets[${petIndex}].species`, {
-  //                     required: true,
-  //                   })}
-  //                   className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-  //                 >
-  //                   <option value="">Select a species</option>
-  //                   <option value="dog">Dog</option>
-  //                   <option value="cat">Cat</option>
-  //                   <option value="bird">Bird</option>
-  //                   <option value="fish">Fish</option>
-  //                   <option value="rabbit">Rabbit</option>
-  //                   <option value="reptile">Reptile</option>
-  //                   <option value="hamster">Hamster</option>
-  //                   <option value="other">Other</option>
-  //                 </select>
-  //               </div>
-
-  //               <div>
-  //                 <label
-  //                   htmlFor={`breed_${petIndex}`}
-  //                   className="block text-sm font-medium text-gray-700"
-  //                 >
-  //                   Breed
-  //                 </label>
-  //                 <select
-  //                   id={`breed_${petIndex}`}
-  //                   {...register(`pets[${petIndex}].breed`, { required: true })}
-  //                   className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-  //                 >
-  //                   <option value="">Select a breed</option>
-  //                   {/* Grouping species and their breeds */}
-  //                   {Object.keys(speciesBreeds)?.map((species) => (
-  //                     <optgroup
-  //                       label={
-  //                         species.charAt(0).toUpperCase() + species.slice(1)
-  //                       }
-  //                       key={species}
-  //                     >
-  //                       {speciesBreeds[species].map((breed) => (
-  //                         <option key={breed} value={breed}>
-  //                           {breed}
-  //                         </option>
-  //                       ))}
-  //                     </optgroup>
-  //                   ))}
-  //                   <option value="Other">Other</option>
-  //                 </select>
-  //               </div>
-
-  //               <div>
-  //                 <label
-  //                   htmlFor={`sex_${petIndex}`}
-  //                   className="block text-sm font-medium text-gray-700"
-  //                 >
-  //                   Sex
-  //                 </label>
-  //                 <select
-  //                   id={`sex_${petIndex}`}
-  //                   {...register(`pets[${petIndex}].sex`, { required: true })}
-  //                   className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-  //                 >
-  //                   <option value="Male">Male</option>
-  //                   <option value="Female">Female</option>
-  //                 </select>
-  //               </div>
-
-  //               <div>
-  //                 <label
-  //                   htmlFor={`color_${petIndex}`}
-  //                   className="block text-sm font-medium text-gray-700"
-  //                 >
-  //                   Color
-  //                 </label>
-  //                 <input
-  //                   type="text"
-  //                   id={`color_${petIndex}`}
-  //                   {...register(`pets[${petIndex}].color`, { required: true })}
-  //                   className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-  //                 />
-  //               </div>
-
-  //               <div>
-  //                 <label
-  //                   htmlFor={`dob_${petIndex}`}
-  //                   className="block text-sm font-medium text-gray-700"
-  //                 >
-  //                   Date of Birth
-  //                 </label>
-  //                 <input
-  //                   type="date"
-  //                   id={`dob_${petIndex}`}
-  //                   {...register(`pets[${petIndex}].dob`, { required: true })}
-  //                   className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-  //                 />
-  //               </div>
-  //               <div className="flex">
-  //                 <label
-  //                   htmlFor={`neutered_${petIndex}`}
-  //                   className="block text-sm font-medium text-gray-700"
-  //                 >
-  //                   Neutered
-  //                 </label>
-  //                 <input
-  //                   type="checkbox"
-  //                   id={`neutered_${petIndex}`}
-  //                   {...register(`pets[${petIndex}].neutered`)}
-  //                   className="mt-1 ml-4 block w-4 h-4 rounded-full border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-  //                 />
-  //               </div>
-  //               <div>
-  //                 <label
-  //                   htmlFor={`registrationDate_${petIndex}`}
-  //                   className="block text-sm font-medium text-gray-700"
-  //                 >
-  //                   Registration date
-  //                 </label>
-  //                 <input
-  //                   type="date"
-  //                   id={`dob_${petIndex}`}
-  //                   {...register(`pets[${petIndex}].registrationDate`, {
-  //                     required: true,
-  //                   })}
-  //                   className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-  //                 />
-  //               </div>
-  //               {pet.vaccinations &&
-  //                 pet.vaccinations.map((vaccination, vaccinationIndex) => (
-  //                   <div
-  //                     key={vaccinationIndex}
-  //                     className="flex items-center gap-4 mb-2"
-  //                   >
-  //                     <input
-  //                       type="text"
-  //                       {...register(
-  //                         `pets[${petIndex}].vaccinations[${vaccinationIndex}].name`
-  //                       )}
-  //                       className="w-1/3 rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-  //                       placeholder="Vaccination Name"
-  //                     />
-  //                     <input
-  //                       type="number"
-  //                       {...register(
-  //                         `pets[${petIndex}].vaccinations[${vaccinationIndex}].numberOfDose`
-  //                       )}
-  //                       className="w-1/4 rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-  //                       placeholder="Doses"
-  //                     />
-  //                     <button
-  //                       type="button"
-  //                       onClick={() =>
-  //                         handleRemoveVaccination(petIndex, vaccinationIndex)
-  //                       }
-  //                       className="text-red-600 hover:text-red-800 text-sm"
-  //                     >
-  //                       Remove
-  //                     </button>
-  //                   </div>
-  //                 ))}
-  //               <div className="mt-2">
-  //                 <button
-  //                   type="button"
-  //                   onClick={() => handleAddVaccination(petIndex)}
-  //                   className="py-1 px-3 border border-transparent rounded-md shadow-sm text-sm font-medium text-indigo-600 bg-indigo-100 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-  //                 >
-  //                   Add Vaccination
-  //                 </button>
-  //               </div>
-  //             </div>
-  //           </div>
-  //         ))}
-
-  //         <button
-  //           type="button"
-  //           onClick={handleAddPet}
-  //           className="mt-4 w-full py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-indigo-600 bg-indigo-100 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-  //         >
-  //           Add Another Pet
-  //         </button>
-
-  //         <div>
-  //           <button
-  //             type="submit"
-  //             className="w-full py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-  //           >
-  //             Submit
-  //           </button>
-  //         </div>
-  //       </form>
-  //     </div>
-  //   );
   if (addPetLoading) {
     return (
       <div className="flex justify-center items-center h-screen bg-gradient-to-br from-[#EFE3C2] to-[#85A947]/10">
@@ -469,27 +155,39 @@ const PetForm = () => {
                   </div>
                 </div>
 
-                <div className="space-y-2">
-                  <label
-                    htmlFor="phone"
-                    className="block text-sm font-bold text-[#3E7B27] mb-2"
-                  >
-                    Phone Number
-                  </label>
-                  <div className="relative">
-                    <input
-                      type="tel"
-                      id="phone"
-                      {...register("phone", {
-                        required: true,
-                        pattern: /^[0-9]{10}$/,
-                      })}
-                      className="w-full px-4 py-4 border-2 border-[#85A947]/30 rounded-xl shadow-sm focus:border-[#3E7B27] focus:ring-4 focus:ring-[#85A947]/20 bg-white/90 backdrop-blur-sm text-[#123524] font-medium placeholder-[#123524]/50 transition-all duration-200"
-                      placeholder="Enter 10-digit phone number"
-                    />
-                    <div className="absolute right-3 top-1/2 transform -translate-y-1/2 w-2 h-2 bg-[#85A947] rounded-full"></div>
-                  </div>
-                </div>
+   <div className="space-y-2">
+  <label
+    htmlFor="phone"
+    className="block text-sm font-bold text-[#3E7B27] mb-2"
+  >
+    Phone Number
+  </label>
+  <div className="relative">
+    <input
+      type="tel"
+      id="phone"
+      maxLength={10}
+      {...register("phone", {
+        required: "Phone number is required",
+        pattern: {
+          value: /^[0-9]{10}$/,
+          message: "Enter a valid 10-digit phone number",
+        },
+      })}
+      className="w-full px-4 py-4 border-2 border-[#85A947]/30 rounded-xl shadow-sm focus:border-[#3E7B27] focus:ring-4 focus:ring-[#85A947]/20 bg-white/90 backdrop-blur-sm text-[#123524] font-medium placeholder-[#123524]/50 transition-all duration-200"
+      placeholder="Phone number"
+    />
+    <div className="absolute right-3 top-1/2 transform -translate-y-1/2 w-2 h-2 bg-[#85A947] rounded-full"></div>
+  </div>
+
+  {/* ✅ Inline error display */}
+  {errors.phone && (
+    <p className="text-red-600 text-sm font-medium mt-1">
+      {errors.phone.message}
+    </p>
+  )}
+</div>
+
 
                 <div className="space-y-2">
                   <label
